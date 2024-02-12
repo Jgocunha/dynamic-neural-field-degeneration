13c79b04708c96da6d94617d2eedc177211e6f8a
joao cunha <gasparcunha19@outlook.pt>
2024-02-12 14:58:28 +0100

    null

--- dynamic-neural-field-degeneration/data/results/matlab-analysis-files/histogram_behaviours.m
+++ dynamic-neural-field-degeneration/data/results/matlab-analysis-files/histogram_behaviours.m
@@ -10,24 +10,30 @@ addpath(genpath(folder));
 
 %% Setup variables
 experiments = {%'deactivate weights', 'weight'; ...
-               %'reduce 0.05 weights', 'weight'; ... 
+               'reduce 0.005 weights', 'weight'; ... 
                %'randomize weights', 'weight'; ...
                %'deactivate pre-synaptic neurons', 'pre-synaptic neuron'; ...
-               'deactivate post-synaptic neurons', 'post synaptic neuron'; ...
+               %'deactivate post-synaptic neurons', 'post synaptic neuron'; ...
                };
 
 positions = {'78.0'; '82.0'; '86.0'; '90.0'; '94.0'; '98.0'; '102.0'};  
 targetCentroids = {78.0; 82.0; 86.0; 90.0; 94.0; 98.0; 102.0}; 
-percentageJumps = {%1000, 10000, 1000, 36,
-    18};
+percentageJumps = {2};
 % 100% - 720 pre-synaptic
 % 100% - 360 post_synaptic
 
+% 100% - 720 * 360 / 1000 = 259.2
+% 0.08% - 1.296
 
 centroidsFilePath = '';
 analysisFilePath = '';
 plotFilePath = './plots/';
 acceptableDeviation = 2.0;
+
+% Define RGB values for dark green, dark yellow, and dark red
+darkGreen = [0, 0.5, 0]; % RGB: [0, 128, 0]
+darkYellow = [0.8, 0.8, 0]; % RGB: [204, 204, 0]
+darkRed = [0.8, 0, 0]; % RGB: [204, 0, 0]
 
 %% Run analysis
 for experiment = 1:size(experiments,1)
@@ -70,10 +76,10 @@ for experiment = 1:size(experiments,1)
         percentage = 0;
         for col = 1:percentageJumps{experiment}:max_length
             disp([num2str(percentage) blanks(9 - length(num2str(col))) num2str(sum_zeros(col)) blanks(9 - length(num2str(sum_zeros(col)))) num2str(sum_ones(col)) blanks(9 - length(num2str(sum_ones(col)))) num2str(sum_twos(col))]);
-            percentage = percentage + 5;
+            percentage = percentage + 0.77;
         end
         
-              % Calculate percentages
+        % Calculate percentages
         total = sum_zeros + sum_ones + sum_twos;
         percentages_zeros = (sum_zeros ./ total) * 100;
         percentages_ones = (sum_ones ./ total) * 100;
@@ -81,29 +87,37 @@ for experiment = 1:size(experiments,1)
         
         
         % Add 100% no behaviors at the next degeneration percentage
-        deg_percentage = 0:percentageJumps{experiment}:((max_length-1)*percentageJumps{experiment});
+        deg_percentage = 0:percentageJumps{experiment}:((max_length-1)*percentageJumps{experiment} + 0);
         extra_percentage = ones(1, numel(deg_percentage)) * 100; % Ensure dimensions match
-        
+
         percentages_zeros(end+1) = 0;
         percentages_ones(end+1) = 0;
         percentages_twos(end+1) = 100;
         deg_percentage(end+1) = deg_percentage(end)+1;
+        
 
-
-        % Define RGB values for dark green, dark yellow, and dark red
-        darkGreen = [0, 0.5, 0]; % RGB: [0, 128, 0]
-        darkYellow = [0.8, 0.8, 0]; % RGB: [204, 204, 0]
-        darkRed = [0.8, 0, 0]; % RGB: [204, 0, 0]
+        % Define the range of the last 10 elements
+        last_ten_indices = max(1, numel(deg_percentage)-20:numel(deg_percentage));
         
         % Plot grouped bar plot with adjusted spacing
-        barWidth = 200; % Adjust bar width
-        spacing = 100; % Adjust spacing between bars
+        barWidth = 1; % Adjust bar width
+        spacing = 1; % Adjust spacing between bars
         figure;
-        bar(deg_percentage - spacing, percentages_zeros, barWidth, 'FaceColor', darkGreen);
+        bar(deg_percentage(last_ten_indices) - spacing, percentages_zeros(last_ten_indices), barWidth, 'FaceColor', darkGreen);
         hold on;
-        bar(deg_percentage, percentages_ones, barWidth, 'FaceColor', darkYellow);
-        bar(deg_percentage + spacing, percentages_twos, barWidth, 'FaceColor', darkRed);
+        bar(deg_percentage(last_ten_indices), percentages_ones(last_ten_indices), barWidth, 'FaceColor', darkYellow);
+        bar(deg_percentage(last_ten_indices) + spacing, percentages_twos(last_ten_indices), barWidth, 'FaceColor', darkRed);
         hold off;
+
+%         % Plot grouped bar plot with adjusted spacing
+%         barWidth = 100; % Adjust bar width
+%         spacing = 100; % Adjust spacing between bars
+%         figure;
+%         bar(deg_percentage - spacing, percentages_zeros, barWidth, 'FaceColor', darkGreen);
+%         hold on;
+%         bar(deg_percentage, percentages_ones, barWidth, 'FaceColor', darkYellow);
+%         bar(deg_percentage + spacing, percentages_twos, barWidth, 'FaceColor', darkRed);
+%         hold off;
 
         
         xlabel('Degeneration Percentage');
