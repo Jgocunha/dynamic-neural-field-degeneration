b0b87ee6e46117fc077dbe1244623711011f15d4
joao cunha <gasparcunha19@outlook.pt>
2024-09-26 09:31:52 +0100

    final results inducing degeneration\

--- dynamic-neural-field-degeneration/data/results/matlab-analysis-files/helper-functions/adapt_to_percentage.m
+++ dynamic-neural-field-degeneration/data/results/matlab-analysis-files/helper-functions/adapt_to_percentage.m
@@ -1,27 +1,27 @@
 function [avgNumIterationsPercentage, avgIterationsMisbehaviorPercentage] = adapt_to_percentage(experiment, avgNumIterations, avgIterationsMisbehavior)
     switch experiment
         case 'deactivate weights'
-            size = 259; 
-        case 'reduce 0.005 weights' 
-            size = 259; 
+            size = 202; 
+        case 'reduce 0.05 weights' 
+            size = 202; 
         case 'randomize weights'
-            size = 259;
+            size = 202;
         case 'deactivate pre-synaptic neurons'
             size = 720;
         case 'deactivate post-synaptic neurons'
-            size = 360;
+            size = 280;
     end
     avgNumIterationsPercentage = (avgNumIterations*100)/size;
     avgIterationsMisbehaviorPercentage = (avgIterationsMisbehavior*100)/size;
 end
 
 % size of perceptual field = 360 / 0.5 = 720
-% size of decision field = 28 / 0.1 = 360
-% number of weights = 720 * 360 = 259,200
+% size of decision field = 28 / 0.1 = 280
+% number of weights = 720 * 280 = 201600
 
 % number of weights deactivated per iteration = 1000
 % number of neurons deactivated per iteration = 1
 
-% size of weights = 201,600/1000 = 259.2
+% size of weights = 201,600/1000 = 201.6
 % size of per neurons = 720/1 = 720
-% size of out neurons = 360/1 = 360
+% size of out neurons = 280/1 = 280
